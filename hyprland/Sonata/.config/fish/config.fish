61dceceed5a411d6092ed3a0d2934713ad671594
hydraPV <crescente.a.cm@gmail.com>
2025-03-06 23:44:59 -0300

    Changed prompt

--- hyprland/Sonata/.config/fish/config.fish
+++ hyprland/Sonata/.config/fish/config.fish
@@ -2,11 +2,7 @@ set -g fish_greeting ''
 
 function fish_prompt
 
-  # interactive user name @ host name, date/time in YYYY-mm-dd format and path
-
-  set_color 404040; echo -n  [(prompt_pwd)] 
-  set_color D9D9D9; echo -n  Hydra
-  set_color E2E2E2; echo -n "     "
-
+    set_color 89c1f5; echo -n [(prompt_pwd)]
+    set_color 404040; echo -n " - "
+    set_color 61b0ff; echo -n " > "
 end
-
