e67b8e31066bdbb1e7a72c22e1f7a9972b2724d2
hydraPV <crescente.a.cm@gmail.com>
2025-03-16 22:27:22 -0300

    Cleaned and added some keybinds

--- hyprland/Gruvbox/.config/nvim/init.lua
+++ hyprland/Gruvbox/.config/nvim/init.lua
@@ -1,35 +1,65 @@
--- Lazy.nvim
+-- Lazy bootstrap
 require("config.lazy")
 
--- General config
-vim.opt.number = true
-vim.opt.cursorline = true
-vim.opt.fillchars = { eob = ' ' }
+--      General Settings
 
--- Disable netrw
+-- Disable netrw (built-in file explorer)
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- Tab
-vim.opt.tabstop = 2        
-vim.opt.shiftwidth = 2    
-vim.opt.softtabstop = 2 
+-- Line numbers config
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.fillchars = { eob = ' ' }
+
+-- Set tab distance
+vim.opt.tabstop = 4        
+vim.opt.shiftwidth = 4    
+vim.opt.softtabstop = 4 
 vim.opt.expandtab = true
 
 
--- Oil.nvim
-vim.keymap.set("n", "<leader>o", require("oil").open) -- Open Oil.nvim
 
--- Nvim-tree.nvim
-vim.keymap.set('n', '<leader>n', ':NvimTreeToggle<CR>', { noremap = true, silent = true }) -- Open/close Nvim-tree.nvim
+--      Keybinds
 
-vim.keymap.set('n', '<leader>e', function()
+-- Set <space> as leader key
+vim.g.mapleader = " "  -- Set space as the leader key
+
+-- Disable the leader key alone (makes <leader> a non-functional prefix)
+vim.keymap.set('n', '<leader>', '<nop>', { noremap = true, silent = true })
+
+-- Disable Arrow Keys in Normal, Insert, and Visual Modes
+vim.keymap.set({ "n", "i", "v" }, "<Up>", "<NOP>", { desc = "Disable Up Arrow" })
+vim.keymap.set({ "n", "i", "v" }, "<Down>", "<NOP>", { desc = "Disable Down Arrow" })
+vim.keymap.set({ "n", "i", "v" }, "<Left>", "<NOP>", { desc = "Disable Left Arrow" })
+vim.keymap.set({ "n", "i", "v" }, "<Right>", "<NOP>", { desc = "Disable Right Arrow" })
+
+-- Move in Insert Mode with <C-hjkl> (better navigation control)
+vim.api.nvim_set_keymap('i', '<C-h>', '<Left>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<C-j>', '<Down>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<C-k>', '<Up>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<C-l>', '<Right>', { noremap = true, silent = true })
+
+
+--      Oil.nvim
+
+-- Open oil.nvim (file explorer)
+vim.keymap.set("n", "<leader>o", require("oil").open)
+
+
+
+--      Nvim-tree
+
+-- Toggle the Nvim-tree file explorer
+vim.keymap.set('n', '<leader>n', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+
+-- Switch between Nvim-tree and the editor (toggle behavior)
+vim.keymap.set('n', '<leader>e', function() 
   local nvim_tree_win_id = vim.fn.bufwinid('NvimTree')
   if nvim_tree_win_id ~= -1 then
-    -- If NvimTree is already open, focus it
     vim.cmd('wincmd p')
   else
-    -- Otherwise, open NvimTree without showing errors if it fails
     local status_ok, _ = pcall(require('nvim-tree').toggle)
     if not status_ok then
       print("Failed to toggle NvimTree")
@@ -38,27 +68,33 @@ vim.keymap.set('n', '<leader>e', function()
 end, { noremap = true, silent = true })
 
 
--- Bufferline.nvim
-vim.keymap.set('n', '<leader>w', ':bdelete<CR>', { noremap = true, silent = true }) -- Close the current tab
+--      Bufferline.nvim
 
-vim.keymap.set('n', '<leader>1', function() require'bufferline'.go_to_buffer(1) end, { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>2', function() require'bufferline'.go_to_buffer(2) end, { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>3', function() require'bufferline'.go_to_buffer(3) end, { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>4', function() require'bufferline'.go_to_buffer(4) end, { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>5', function() require'bufferline'.go_to_buffer(5) end, { noremap = true, silent = true })
+-- Close the current buffer (tab)
+vim.keymap.set('n', '<leader>w', ':bdelete<CR>', { noremap = true, silent = true })
 
--- Bind to block arrow keys
-vim.keymap.set({ "n", "i", "v" }, "<Up>", "<NOP>", { desc = "Disable Up Arrow" })
-vim.keymap.set({ "n", "i", "v" }, "<Down>", "<NOP>", { desc = "Disable Down Arrow" })
-vim.keymap.set({ "n", "i", "v" }, "<Left>", "<NOP>", { desc = "Disable Left Arrow" })
-vim.keymap.set({ "n", "i", "v" }, "<Right>", "<NOP>", { desc = "Disable Right Arrow" })
-vim.keymap.set('n', '<leader>', '<nop>', { noremap = true, silent = true })
+-- Switch between buffers (1 to 5) using <leader> key
+for i = 1, 5 do
+  vim.keymap.set('n', '<leader>'..i, function() require'bufferline'.go_to_buffer(i) end, { noremap = true, silent = true })
+end
 
 
--- Bind <C-hjkl> to move in insert mode
-vim.api.nvim_set_keymap('i', '<C-h>', '<Left>', { noremap = true, silent = true })  
-vim.api.nvim_set_keymap('i', '<C-j>', '<Down>', { noremap = true, silent = true }) 
-vim.api.nvim_set_keymap('i', '<C-k>', '<Up>', { noremap = true, silent = true })  
-vim.api.nvim_set_keymap('i', '<C-l>', '<Right>', { noremap = true, silent = true })
+--      Snacks.nvim
 
+-- Search through open buffers using Snacks.nvim
+vim.keymap.set('n', "<leader>,", function() Snacks.picker.buffers() end)
 
+-- Open git log
+vim.keymap.set('n', "<leader>gl", function() Snacks.picker.git_log() end)
+
+-- Toggle Zen Mode
+vim.keymap.set('n', "<leader>z",  function() Snacks.zen() end)
+
+-- Open current git repo in browser
+vim.keymap.set('n', "<leader>gz", function() Snacks.gitbrowse() end)
+
+-- Open lazygit
+vim.keymap.set('n', "<leader>gg", function() Snacks.lazygit() end)
+
+-- Color scheme selector
+vim.keymap.set('n', "<leader>uC", function() Snacks.picker.colorschemes() end)
