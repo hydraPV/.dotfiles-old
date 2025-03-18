75ee6d3fc00a21545700ff43a432f4c6f854adf5
hydraPV <160309620+hydraPV@users.noreply.github.com>
2025-03-18 20:57:18 -0300

    Changed prompt

--- hyprland/Gruvbox/.config/fish/config.fish
+++ hyprland/Gruvbox/.config/fish/config.fish
@@ -2,6 +2,6 @@ set -g fish_greeting ''
 set -U fish_prompt_pwd_dir_length 4
 function fish_prompt
     echo " "
-    set_color 7ba7c2; echo " 󰉓  "(prompt_pwd)  
+    set_color 7ba7c2; echo "["(date "+%H:%M")"]" "--> 󰉓 ""["(prompt_pwd)"]"
     set_color d0e3ea; echo -n " Hydra "
 end
