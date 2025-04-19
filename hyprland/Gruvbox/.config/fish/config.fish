41f07a1469dd943cd5151a9fcda139d6193711a9
hydraPV <160309620+hydraPV@users.noreply.github.com>
2025-04-19 02:38:01 -0400

    .

--- hyprland/Gruvbox/.config/fish/config.fish
+++ hyprland/Gruvbox/.config/fish/config.fish
@@ -1,5 +1,6 @@
 set -g fish_greeting ''
 set -U fish_prompt_pwd_dir_length 4
+set -x GTK_THEME Gruvbox
 function fish_prompt
     echo " "
     set_color 7ba7c2; echo "["(date "+%H:%M")"]" "--> 󰉓 ""["(prompt_pwd)"]"
